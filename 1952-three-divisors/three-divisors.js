5593b56c0362511e7df80c04450a464c1c446291
roshan <roshan@roshans-MacBook-Pro.local>
2024-05-07 12:02:34 +1000

    correct problem number

--- 1952-three-divisors/three-divisors.js
+++ 1952-three-divisors/three-divisors.js
@@ -3,12 +3,26 @@
  * @return {boolean}
  */
 var isThree = function(n) {
-    let count = 2;
-    for (let i=2; i<n; i++) {
-        if (n%i==0) {
-            count++;
+    if ([1,2,3].includes(n)) return false;
+    // if perfect square and the sqaure root is prime,it has 3 divisors exactly
+    let sqrRoot = Math.sqrt(n)
+    if (sqrRoot%1==0) {
+        for (let i = 2; i < sqrRoot; i++) {
+            if (sqrRoot % i == 0) {
+                return false;
+            }
         }
-        if (count > 3) return false;
+        return true;
     }
-    return count == 3 ? true : false;
+    return false;
+
+    // Old Solution
+    // let count = 2;
+    // for (let i=2; i<n; i++) {
+    //     if (n%i==0) {
+    //         count++;
+    //     }
+    //     if (count > 3) return false;
+    // }
+    // return count == 3 ? true : false;
 };
