02e096d15788de95a0d3fbba45c2bfb74a32d2a0
roshan101 <roshanbudhathoki02@gmail.com>
2024-05-10 10:21:31 +1000

    Time: 59 ms (70.73%) | Memory: 50.7 MB (46.29%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,11 +5,18 @@
  */
 
 var twoSum = function(nums, target) {
+    // for (let i=0;i<nums.length;i++) {
+    //     for(let j=i+1;j<nums.length;j++) {
+    //        if(nums[i] + nums[j] == target) {
+    //            return [i,j]
+    //        } 
+    //     }   
+    // }
+    let hashTable = {};
     for (let i=0;i<nums.length;i++) {
-        for(let j=i+1;j<nums.length;j++) {
-           if(nums[i] + nums[j] == target) {
-               return [i,j]
-           } 
-        }   
+        if (hashTable[nums[i]] != null) {
+            return[hashTable[nums[i]], i]
+        }
+        hashTable[target-nums[i]] = i;
     }
 };
