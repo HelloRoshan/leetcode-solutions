0ec2da745598b275e5cae6240ef4b6ec389fd920
roshan101 <roshanbudhathoki02@gmail.com>
2024-05-21 16:43:45 +1000

    Time: 64 ms (33.07%) | Memory: 50.7 MB (93.76%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    let start = 0;
+    let end = image.length - 1;
+    let row = 0;
+    while (start <= end && row < image.length) {
+        let temp = !!image[row][start] ? 0 : 1;
+        image[row][start] = !!image[row][end] ? 0 : 1;
+        image[row][end] = temp;
+        start++;
+        end--;
+        if(start> end) {
+            row++;
+            start = 0;
+            end = image.length - 1;
+        }
+    }
+    return image;
+};
